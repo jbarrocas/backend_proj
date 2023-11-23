782fd3472334dc48b315d09796ae965c6b3836c2
José Barrocas <128058496+jbarrocas@users.noreply.github.com>
2023-11-23 21:01:58 +0000

    Admin Dashboard Page css

--- views/dashboard.php
+++ views/dashboard.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
     <title>Admin Dashboard</title>
 </head>
 <body>
@@ -10,31 +11,33 @@
 <?php
 require("templates/adminmenu.php");
 ?>
-        <h1>Admin Dashboard</h1>
-        <div id="AdminActionArea">            
-            <section class="admin-action-area">
-                <h2>Admin Action Area</h2>
-                <ul>
-                    <li><a href="/admin_postreports/">Posts Reports</a></li>
-                    <li><a href="/admin_commentreports/">Comments Reports</a></li>
-                    <li><a href="/admin_report/">Report to Administration</a></li>
-                </ul>
-            </section>
+        <div class="page-content">
+            <h1 class="heading-1">Admin Dashboard</h1>
+            <div id="AdminActionArea">            
+                <section class="admin-action-area">
+                    <h2 class="heading-2">Admin Action Area</h2>
+                    <ul>
+                        <li class="admin-blue-button"><a href="/admin_postreports/">Posts Reports</a></li>
+                        <li class="admin-white-button"><a href="/admin_commentreports/">Comments Reports</a></li>
+                        <li class="admin-red-button"><a href="/admin_report/">Report to Administration</a></li>
+                    </ul>
+                </section>
 <?php
     if(isset($_SESSION["is_super_admin"])) {
         echo '
             <section class="super-admin-area">
-                <h2>Super Admin Area</h2>
+                <h2 class="heading-2">Super Admin Area</h2>
                 <ul>
-                    <li><a href="/admin_list/">Admins List</a></li>
-                    <li><a href="/admin_reports/">Admins Reports</a></li>
-                    <li><a href="/admin_search_user/">Search User</a></li>
-                    <li><a href="/admin_accountreports/">Deleted Accounts Reports</a></li>
+                    <li class="admin-blue-button"><a href="/admin_list/">Admins List</a></li>
+                    <li class="admin-white-button"><a href="/admin_reports/">Admins Reports</a></li>
+                    <li class="admin-red-button"><a href="/admin_search_user/">Search User</a></li>
+                    <li class="admin-blue-button"><a href="/admin_accountreports/">Deleted Accounts Reports</a></li>
                 </ul>
             </section>
         ';
     }
 ?>
+            </div>
         </div>
 <?php
     require("templates/footer.php");
