ce4348b3e4126c0b69c14678ce62da98773d2b1b
José Barrocas <128058496+jbarrocas@users.noreply.github.com>
2023-11-23 15:09:15 +0000

    Update password page css

--- views/updatepassword.php
+++ views/updatepassword.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
     <title>Update Password</title>
 </head>
 <body>
@@ -15,18 +16,28 @@
         require("templates/menu.php");
     }
 ?>
-        <h1>Update your password</h1>
+        <div class="page-content">
+            <h1 class="heading-1">Update your password</h1>
 <?php
     if( isset($message)) {
         echo ' <p role="alert">' .$message.' </p>';
     }
-?>       
-        <form method="POST" action="/updatepassword/">
-            <input class="register-input" type="password" name="old_password" placeholder="Old Password" required minlength="8" maxlength="1000">
-            <input class="register-input" type="password" name="new_password" placeholder="New Password" required minlength="8" maxlength="1000">
-            <input class="register-input" type="password" name="new_password_confirm" placeholder="New Password Confirmation" required minlength="8" maxlength="1000">
-            <button type="submit" name="send">Update Password</button>
-        </form>
+?>
+            <form class="form" method="POST" action="/updatepassword/">
+                <div>
+                    <input class="form-input" type="password" name="old_password" placeholder="Old Password" required minlength="8" maxlength="1000">
+                </div>
+                <div>
+                    <input class="form-input" type="password" name="new_password" placeholder="New Password" required minlength="8" maxlength="1000">
+                </div>
+                <div>
+                    <input class="form-input" type="password" name="new_password_confirm" placeholder="New Password Confirmation" required minlength="8" maxlength="1000">
+                </div>
+                <div>
+                    <button class="form-button" type="submit" name="send">Update Password</button>
+                </div>
+            </form>
+        </div>
 <?php
     require("templates/footer.php");
 ?>
