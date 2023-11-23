326b888981d96c4a187b5f36d6650efef66ec804
José Barrocas <128058496+jbarrocas@users.noreply.github.com>
2023-11-23 15:04:02 +0000

    Update User details page css

--- views/updatedetails.php
+++ views/updatedetails.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
     <title>User Details - <?= $user["username"] ?></title>
 </head>
 <body>
     <main>
-        <div class="user-details-container">
 <?php
     if(isset($_SESSION["is_admin"]) || isset($_SESSION["is_super_admin"])) {
         require("templates/adminmenu.php");
@@ -16,17 +16,25 @@
         require("templates/menu.php");
     }
 ?>
-            <div class="form-container">
+        <div class="page-content">
+            <h1 class="heading-1">Update Your Details</h1>
 <?php
     if( isset($message) ) {
         echo '<p role="alert">' .$message. '</p>';
     }
 ?>
-                <form class="details-form" method="POST" action="/updatedetails/">
-                    <input class="details-input" type="text" name="first_name" placeholder="First Name" value="<?= $user["first_name"] ?>" required minlength="3" maxlength="22">
-                    <input class="details-input" type="text" name="last_name" placeholder="Last Name" value="<?= $user["last_name"] ?>" required minlength="2" maxlength="22">
-                    <input class="details-input" type="email" name="email" placeholder="Email" value="<?= $user["email"] ?>" required>
-                    <select class="details-select" name="country_id" required>
+            <form class="form" class="details-form" method="POST" action="/updatedetails/">
+                <div>
+                    <input class="form-input" type="text" name="first_name" placeholder="First Name" value="<?= $user["first_name"] ?>" required minlength="3" maxlength="22">
+                </div>
+                <div>
+                    <input class="form-input" type="text" name="last_name" placeholder="Last Name" value="<?= $user["last_name"] ?>" required minlength="2" maxlength="22">
+                </div>
+                <div>
+                    <input class="form-input" type="email" name="email" placeholder="Email" value="<?= $user["email"] ?>" required>
+                </div>
+                <div>
+                    <select class="form-input" name="country_id" required>
 <?php
     foreach($countries as $country) {
         $userCountry = $user["country_id"];
@@ -38,15 +46,17 @@
     }
 ?>
                     </select>
-                    <button class="update-button" type="submit" name="send">Update Details</button>
-                </form>
-                <p><a href="/updatepassword/">Update Password</a></p>
+                </div>
+                <div>
+                    <button class="form-button" type="submit" name="send">Update Details</button>
+                </div>
+            </form>
+            <a href="/updatepassword/"><div class="form-whiteButton">Update Password</div></a>
 <?php
     if( isset($message) ) {
         echo '<p role="alert">' . $message . '</p>';
     }
 ?>
-            </div>
         </div>
 <?php
     require("templates/footer.php");
