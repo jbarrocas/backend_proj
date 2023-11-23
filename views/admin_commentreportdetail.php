e891d208b114c3bf6179d99de584e13096d83d5b
José Barrocas <128058496+jbarrocas@users.noreply.github.com>
2023-11-23 21:19:00 +0000

    Comment Report Detail Page CSS

--- views/admin_commentreportdetail.php
+++ views/admin_commentreportdetail.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
     <title>Comment Report Detail</title>
 </head>
 <body>
@@ -10,22 +11,24 @@
 <?php
     require("templates/adminmenu.php");
 ?>
-        <h1>Comment Report Detail</h1>
-        <div class="reported-comment">
-            <p>Content: <?=$commentReport["content"]?></p>
-            <p>Author: <?=$user["username"]?></p>
-            <p>Report Subject: <?=$commentReport["name"]?></p>
-            <div class="form-container">
-                <form method="post" action="/admin_commentreportdetail/<?= $commentReport["comment_id"] ?>">
-                    <button type="submit" name="dismiss">Dismiss Report</button>
-                </form>
-                <form method="post" action="/admin_commentreportdetail/<?= $commentReport["comment_id"] ?>">
-                    <button type="submit" name="restrict_privileges">Restrict User</button>
-                </form>
-                <form method="post" action="/admin_commentreportdetail/<?= $commentReport["comment_id"] ?>">
-                    <button type="submit" name="ban_user">Ban User</button>
-                </form>
-            </div>
+        <div class="page-content">
+            <h1 class="heading-1">Comment Report Detail</h1>
+                <div class="reported-comment">
+                    <p class="report-content">Content: <?=$commentReport["content"]?></p>
+                    <p class="report-content">Author: <?=$user["username"]?></p>
+                    <p class="report-content">Report Subject: <?=$commentReport["name"]?></p>
+                    <div class="form">
+                        <form method="post" action="/admin_commentreportdetail/<?= $commentReport["comment_id"] ?>">
+                            <button class="form-button" type="submit" name="dismiss">Dismiss Report</button>
+                        </form>
+                        <form method="post" action="/admin_commentreportdetail/<?= $commentReport["comment_id"] ?>">
+                            <button class="form-white-button" type="submit" name="restrict_privileges">Restrict User</button>
+                        </form>
+                        <form method="post" action="/admin_commentreportdetail/<?= $commentReport["comment_id"] ?>">
+                            <button class="form-red-button" type="submit" name="ban_user">Ban User</button>
+                        </form>
+                    </div>
+                </div>
         </div>
 <?php
     require("templates/footer.php");
