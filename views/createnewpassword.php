8a551799d7821729f35df9bb934859d175f0bad8
José Barrocas <128058496+jbarrocas@users.noreply.github.com>
2023-11-23 18:12:18 +0000

    Create new password page css

--- views/createnewpassword.php
+++ views/createnewpassword.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
     <title>Create New Password</title>
     <script src="/js/form_hide.js"></script>
 </head>
@@ -16,17 +17,25 @@
         require("templates/menu.php");
     }
 ?>
-        <h1>Create new password</h1>
-<?php
-    if( isset($message)) {
-        echo ' <p id="message" role="alert">' .$message.' </p>';
-    }
-?>       
-        <form method="POST" action="/createnewpassword/?token=<?= $_GET["token"] ?>&email=<?= $_GET["email"] ?>" name="form">
-            <input class="register-input" type="password" name="new_password" placeholder="New Password" required minlength="8" maxlength="1000">
-            <input class="register-input" type="password" name="new_password_confirm" placeholder="New Password Confirmation" required minlength="8" maxlength="1000">
-            <button type="submit" name="createpassword">Create new password</button>
-        </form>
+        <div class="page-content">
+            <h1 class="heading-1">Create a new password</h1>
+        <?php
+            if( isset($message)) {
+                echo ' <p class="error-message" id="message" role="alert">' .$message.' </p>';
+            }
+        ?>       
+            <form class="form" method="POST" action="/createnewpassword/?token=<?= $_GET["token"] ?>&email=<?= $_GET["email"] ?>" name="form">
+            <div>
+                <input class="form-input" type="password" name="new_password" placeholder="New Password" required minlength="8" maxlength="1000">
+            </div>
+            <div>
+                <input class="form-input" type="password" name="new_password_confirm" placeholder="New Password Confirmation" required minlength="8" maxlength="1000">
+            </div>
+            <div>
+                <button class="form-button" type="submit" name="createpassword">Create new password</button>
+            </div>
+            </form>
+        </div>
 <?php
     require("templates/footer.php");
 ?>
