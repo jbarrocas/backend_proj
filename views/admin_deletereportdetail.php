33c60f10dbdd022f185309a4cd05e83c5a6e2118
José Barrocas <128058496+jbarrocas@users.noreply.github.com>
2023-11-24 10:22:56 +0000

    Deleted Account Report Detail page CSS

--- views/admin_deletereportdetail.php
+++ views/admin_deletereportdetail.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
     <title>Delete Report Detail</title>
 </head>
 <body>
@@ -10,11 +11,13 @@
 <?php
     require("templates/adminmenu.php");
 ?>
-        <h1>Deleted Account Report Detail</h1>
-        <div class="delete-report">
-            <p>Subject: <?=$report["name"]?></p>
-            <p>Message: <?=$report["user_text"]?></p>
-            <p>Deleted at: <?=$report["deleted_at"]?></p>
+        <div class="page-content">
+            <h1 class="heading-1">Deleted Account Report Detail</h1>
+            <div class="delete-report">
+                <p>Subject: <?=$report["name"]?></p>
+                <p>Message: <?=$report["user_text"]?></p>
+                <p>Deleted at: <?=$report["deleted_at"]?></p>
+            </div>
         </div>
 <?php
     require("templates/footer.php");
